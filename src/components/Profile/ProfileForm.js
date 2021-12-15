464d63cabdd76e738c5508e05f96e0e1a519dbf0
mangelpt <mg20101994@gmail.com>
2021-12-14 22:35:17 -0500

    added http validations

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -4,23 +4,32 @@ import useForm from "../../hooks/useForm";
 import {URl_CHANGE_PASS} from "../../Constants";
 import {useContext} from "react";
 import authContext from "../../store/AuthContext";
-
+import {useHistory} from "react-router-dom";
 
 const ProfileForm = () => {
-
     const [loading, sendRequest] = useHTTP();
     const [formData, inputBlurHandler, , passwordHandler, reset] = useForm();
     const authCtx = useContext(authContext);
-
-    console.log(formData);
+    const history = useHistory();
     const sendRequestHandler = async (e) => {
-      e.preventDefault();
-      const data = await sendRequest({
-          url:URl_CHANGE_PASS ,
-          method:'POST' ,
-          headers:{'Content-Type': 'application/json'},
-          body:{idToken:authCtx.token ,password:formData.password}
-      })
+        e.preventDefault();
+        const data = await sendRequest({
+            url: URl_CHANGE_PASS,
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: {
+                idToken: authCtx.token,
+                password: formData.password,
+                returnSecureToken: false
+            }
+        });
+        if (data.error !== undefined) {
+            alert(data.error.errors[0].message);
+        } else {
+            alert(` password changed`);
+            reset()
+            history.replace("/")
+        }
     }
 
     return (
@@ -34,11 +43,12 @@ const ProfileForm = () => {
                     onBlur={inputBlurHandler}
                     onChange={passwordHandler}
                 />
-                { !formData.passwordIsValid  && formData.isTouched && <p>at least 6 characters</p>}
+                {!formData.passwordIsValid && formData.isTouched && <p>at least 6 characters</p>}
             </div>
             <div className={classes.action}>
                 <button disabled={!formData.passwordIsValid}>Change Password</button>
             </div>
+            {loading && <span>sending</span>}
         </form>
     );
 }
